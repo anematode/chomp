9086eb2502ac9f033377bedee190ae9437664eb9
Timothy Herchen <timothy.herchen@gmail.com>
2021-05-11 17:55:32 -0700

    initial stuff

--- test.js
+++ test.js
@@ -1,20 +1,8 @@
-
-const buffer = new ArrayBuffer(8)
-const arr = new Float64Array(buffer)
-const int32arr = new Uint32Array(buffer)
-
-function getFloatFromParts(a, b) {
-  int32arr[0] = a
-  int32arr[1] = b
-
-  return arr[0]
-}
-
-let seed = 0
-
-// Credit to https://stackoverflow.com/a/52171480/13458117. This is a 53-bit hash using floats, meaning it can
+// Credit to https://stackoverflow.com/a/52171480/13458117. This is a 53-bit hash using floats in some tricky ways,
+// which reduces the hash collision probability near certainty to rather unlikely, for our given space. The C++ code
+// uses a 64-bit hash which is even more robust; that's necessary given its larger search space.
 function hashPosition (position) {
-  let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+  let h1 = 0xdeadbeef, h2 = 0x41c6ce57;
 
   for (let i = 0, value; i < position.length; i++) {
     value = position[i]
@@ -27,14 +15,56 @@ function hashPosition (position) {
   return 4294967296 * (2097151 & h2) + (h1>>>0);
 }
 
-let nan1 = getFloatFromParts(0, 2146959360)
-let nan2 = getFloatFromParts(0, 2146959361)
+// Consider the following position and given cut. We use the following indexing:
 
-let map = new Map()
+//   ###
+//   ### ###
 
-map.set(nan1, "hi")
-map.set(nan2, "bye")
+function getPositionWidth (position) {
+  return position ? position[0] : 0
+}
 
-console.log(Object.is(nan1, nan2))
+function getPositionHeight (position) {
+  return position.length
+}
 
+function getPositionDims (position) {
+  return { width: getPositionWidth(position), height: getPositionHeight(position) }
+}
 
+function prettyPrintPosition (position, {
+  tileCharacter = '#',
+  spaceCharacter = ' ',
+  includeLabels = true,
+  tileWidth = 3,
+  tileHeight = 2,
+  columnSpace = 1,
+  rowSpace = 1
+} = {}) {
+  let { width, height } = getPositionDims(position)
+
+  if (width === 0) return "Empty board."
+
+  // We use an array of an array of an array of strings. The strings look like "####" and these are combined.
+  const strings = []
+
+  const tileString = [...new Array(tileHeight).keys()].map(() => tileCharacter.repeat(tileWidth))
+  const emptyString = [...new Array(tileHeight).keys()].map(() => spaceCharacter.repeat(tileWidth))
+
+  for (let row = 0; row < height; ++row) {
+    const rowString = []
+    let rowWidth = position[row]
+
+    for (let col = 0; col < width; ++col) {
+      rowString.push((col < rowWidth) ? tileString : emptyString)
+    }
+
+    strings.push(rowString)
+  }
+
+  // Convert the string array array array into a string
+  
+
+  // If labels
+  return strings
+}
