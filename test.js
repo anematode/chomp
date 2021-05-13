b3828d74ff518c696df28fa97213c92b14eb5e8c
Timothy Herchen <timothy.herchen@gmail.com>
2021-05-13 10:38:21 -0700

    eg

--- test.js
+++ test.js
@@ -32,6 +32,24 @@ function getPositionDims (position) {
   return { width: getPositionWidth(position), height: getPositionHeight(position) }
 }
 
+function getPositionSquareCount (position) {
+  let sum = 0
+  for (let i = 0; i < position.length; ++i {
+    sum += position[i]
+  }
+  return sum
+}
+
+function restrictStringTo (str, length) {
+  if (str.length === length) {
+    return str
+  } else if (str.length > length) {
+    return str.substr(0, length)
+  } else {
+    return str + ' '.repeat(length - str.length)
+  }
+}
+
 function prettyPrintPosition (position, {
   tileCharacter = '#',
   spaceCharacter = ' ',
@@ -46,7 +64,7 @@ function prettyPrintPosition (position, {
   if (width === 0) return "Empty board."
 
   // We use an array of an array of an array of strings. The strings look like "####" and these are combined.
-  const strings = []
+  let strings = []
 
   const tileString = [...new Array(tileHeight).keys()].map(() => tileCharacter.repeat(tileWidth))
   const emptyString = [...new Array(tileHeight).keys()].map(() => spaceCharacter.repeat(tileWidth))
@@ -62,9 +80,147 @@ function prettyPrintPosition (position, {
     strings.push(rowString)
   }
 
+  function generateColLabel(label) {
+    label = '' + label
+
+    let stringArr = emptyString.slice()
+    let lastIndex = stringArr.length - 1
+
+    let string = Array.from(stringArr[lastIndex])
+
+    let startSubindex = Math.floor(string.length / 2 - label.length / 2)
+    for (let i = startSubindex, j=0; i < startSubindex + label.length; ++j, ++i) {
+      string[i] = label[j]
+    }
+
+    stringArr[lastIndex] = string.join('')
+
+    return stringArr
+  }
+
+  function generateRowLabel(label) {
+    label = '' + label
+
+    let stringArr = emptyString.slice()
+    let subrowIndex = Math.floor(height / 2)
+
+    let subrowString = ' '.repeat(tileWidth - label.length) + label
+
+    stringArr[subrowIndex] = subrowString
+
+    return stringArr
+  }
+
+
+  if (includeLabels) {
+    // Prepend an extra row to the string containing all of the column numbers
+
+    const extraRow = [ emptyString ].concat([...new Array(width).keys()].map(i => generateColLabel(i + 1)))
+
+    for (let i = 0; i < height; ++i) {
+      const row = strings[i]
+
+      row.unshift(generateRowLabel(i + 1))
+    }
+
+    strings.push(extraRow)
+  }
+
+  strings = strings.reverse()
+
   // Convert the string array array array into a string
-  
+
+  let out = []
+
+  for (let row of strings) {
+    let rowOut = []
+
+    for (let i = 0; i < tileHeight; ++i) {
+      const subrowOut = []
+
+      for (let col of row) {
+        subrowOut.push(restrictStringTo(col[i], tileWidth))
+      }
+
+      rowOut.push(subrowOut.join(' '.repeat(columnSpace)))
+    }
+
+    rowOut = rowOut.join('\n')
+
+    out.push(rowOut)
+  }
+
+  out = out.join('\n'.repeat(rowSpace + 1))
 
   // If labels
-  return strings
+  return out
+}
+
+// Map between <hash(position), { winning: true/false }
+const PositionData = new Map()
+
+function getDataOf(position) {
+  return PositionData.get(hashPosition(position))
+}
+
+function setDataOf(position, data) {
+  PositionData.set(hashPosition(position), data)
+}
+
+// credit to geeks for geeks
+function* positionsWithNTiles(n)  {
+  let partition = [n]
+
+  // Length of the current partition - 1
+  let len = 0
+
+  while (true) {
+    yield partition.slice()
+
+    let rem_val = 0
+
+    while (len >= 0 && partition[len] === 1)
+    {
+      rem_val += partition[len]
+      len--
+    }
+
+    if (len < 0)
+      return
+
+    partition[len]--
+    rem_val++
+    while (rem_val > partition[len])
+    {
+      partition[len + 1] = partition[len]
+      rem_val = rem_val - partition[len]
+      len++
+    }
+    partition[len + 1] = rem_val
+    len++
+  }
+}
+
+function* positionsStartingFromPositionWithNTiles (position, n) {
+  let positionSquareCount = getPositionSquareCount(position)
+
+  if (n.length > positionSquareCount) {
+    return
+  }
+
+  const currentPotentialPosition = []
+
+  // The strategy is to recursively iterate through all output_position[0] starting from position[0] down to 0, keeping
+  // track of how many squares have already been used up, and comparing at each point to position[n]. We need to
+  // allocate a certain minimum of tiles each time or we will end up with excess squares. If we are finding the value
+  // of output_position[i] we need to know
+
+  function  (i) {
+
+  }
+}
+
+// Could do via memoization, but... eh...
+function* positionsStartingFrom (position) {
+  // We start from the initial position and generate all
 }
